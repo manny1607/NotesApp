8555d216595b94fa4e6e92f9f62eb8908c8ef2b4
Emanuel Ledesma <ledesma.emanuel.1607@gmail.com>
2019-06-09 20:52:47 -0500

    app finished

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,33 @@
 <template>
   <div id="app">
     <div class="container">
-      <h1>Notes app</h1>
+      <h1>Beautiful Notes App</h1>
       <progress-bar :numNotes="notes.length"></progress-bar>
       <div class="row mt-3">
         <div class="col-md-6 offset-md-3">
           <div class="form-group">
             <label>Title</label>
-            <input class="form-control" type="text" placeholder="Title">
+            <input v-model="titleInput" class="form-control" type="text" placeholder="Title">
             <label>Subtitle</label>
-            <input class="form-control" type="text" placeholder="Subtitle">
+            <input v-model="subtitleInput" class="form-control" type="text" placeholder="Subtitle">
             <label>Write your note</label>
-            <textarea class="form-control" rows="3"></textarea>
-            <button @click="addNote()" class="btn btn-success">Add Note</button>
+            <textarea v-model="textInput" class="form-control" rows="3"></textarea>
+            <button @click="addNote()" class="btn btn-success mt-2">Add Note</button>
           </div>
         </div>
       </div>
 
+      <div class="alert alert-danger" v-if="notes.length == 10">
+        You can't add more notes
+      </div>
+
       <div class="row">
         <div class="col-md-4" :key="note.id" v-for='note in notes'>
-          <note-card></note-card>
+          <note-card :noteId="note.id" @click.native="deleteNote(note.id)">
+            <h5 slot="title" class="card-title">{{note.title}}</h5>
+            <h6 slot="subtitle" class="card-subtitle mb-2 text-muted">{{note.subtitle}}</h6>
+            <p slot="text" class="card-text">{{note.text}}</p>
+          </note-card>
         </div>
       </div>
       
@@ -34,6 +42,9 @@ export default {
   name: 'app',
   data () {
     return {
+      titleInput: '',
+      subtitleInput: '',
+      textInput: '',
       notes: [
         {id:1, title:'Card Title', subtitle:'Card subtitle', text:'Some quick example text to build on the card title and make up the bulk of the card\'s content.'}
       ]
@@ -41,14 +52,27 @@ export default {
   },
   methods:{
     addNote(){
-      var noteId = this.notes.length + 1; 
-      this.notes.push({
-        id:noteId,
-        title:'Another Card',
-        subtitle: 'Another sub',
-        text: 'Blabblablabalbalfaijsdoicwaiopoisñoixñvnpxufxefxhoiryeaafnoxfyoaifapsc'
-      })
-      console.log(this.notes)
+      if(this.notes.length !== 10){
+        this.notes.push({
+          id:this.notes.length + 1,
+          title: this.titleInput,
+          subtitle: this.subtitleInput,
+          text: this.textInput
+        })
+        this.emptyInputs();  
+      }
+    },
+    emptyInputs(){
+      this.titleInput = '';
+      this.subtitleInput = '';
+      this.textInput = '';
+    },
+    deleteNote(noteId){
+      this.notes.forEach( function(element, index, array) {
+        if (element.id == noteId) {
+          array.splice(index,1)
+        }
+      });
     }
   },
   components:{
--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,12 +1,24 @@
 <template>
 	<div class="card mb-4">
 	  <div class="card-body">
-	    <h5 class="card-title">Card title</h5>
-	    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-	    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	  	<slot name="title">
+	  		<!-- <h5 class="card-title">Card title</h5> -->
+	  	</slot>
+	  	<slot name="subtitle">
+	  		<!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
+	  	</slot>
+	  	<slot name="text">
+	  		<!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+	  	</slot>
 	  </div>
 	</div>
 </template>
+
+<script>
+	export default{
+		props: ['noteId']
+	}
+</script>
 
 <style>
 	.card{
