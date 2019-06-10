71da3977130aa83f18497b8721620d053cf61e08
Emanuel Ledesma <ledesma.emanuel.1607@gmail.com>
2019-06-09 19:40:46 -0500

    skeleton setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
 <template>
   <div id="app">
+    <div class="container">
+      <h1>Notes app</h1>
+      <progress-bar></progress-bar>
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <div class="form-group">
+            <label>Write your note</label>
+            <textarea class="form-control" rows="3"></textarea>
+            <button class="btn btn-success">Add Note</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <note-card></note-card>
+        </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
+import ProgressBar from './components/ProgressBar.vue';
+import NoteCard from './components/NoteCard.vue';
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      notes: [
+        {id:1, title:'Card Title', subtitle:'Card subtitle', text:'Some quick example text to build on the card title and make up the bulk of the card\'s content.'}
+      ]
     }
+  },
+  components:{
+    ProgressBar,
+    NoteCard
   }
 }
 </script>
 
 <style>
+body{
+  margin-top: 20px;
+}
 </style>
---
+++ src/components/Progressbar.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="progress">
+	  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">1/10</div>
+	</div>
+</template>
